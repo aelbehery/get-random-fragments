bef7ef339f1b3bc52e07d3f502575081b83a3f8d
aelbehery <aelbehery@aucegypt.edu>
2018-05-14 14:26:58 +0200

    Update subsample.py

--- subsample.py
+++ subsample.py
@@ -4,15 +4,20 @@ import sys
 import random
 
 selected = []
-first_record = SeqIO.parse(sys.argv[1], "fasta").next()
+gen = SeqIO.parse(sys.argv[1], "fasta")
+#filter out all records with one or more Ns
+gen_wo_N = (record for record in gen if "N" not in record.seq)
+first_record = gen_wo_N.next()
 genome = "_".join(first_record.description.split("_")[:-1])
 records = [first_record]
-for record in SeqIO.parse(sys.argv[1], "fasta"):
-    if "_".join(record.description.split("_")[:-1]) == genome and "N" not in record.seq:
+
+for record in gen_wo_N:
+    if "_".join(record.description.split("_")[:-1]) == genome:
         records.append(record)
     else:
         selected.append(random.choice(records))
         genome = "_".join(record.description.split("_")[:-1])
         records = [record]
+
 selected.append(random.choice(records))
 SeqIO.write(selected, sys.argv[2], "fasta")
